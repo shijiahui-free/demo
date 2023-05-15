dd27299f22916645b6453150c1f89a06bab1d56e
shijiahui-free <hmx123456@163.com>
2023-05-15 16:09:24 +0800

    广告报销

--- main/java/kd/bos/LX/GglxFormPlugin.java
+++ main/java/kd/bos/LX/GglxFormPlugin.java
@@ -1,29 +1,25 @@
 package kd.bos.LX;
 
 
-import com.grapecity.documents.excel.S;
 import kd.bos.bill.AbstractBillPlugIn;
-import kd.bos.coderule.api.CodeRuleInfo;
+import kd.bos.dataentity.OperateOption;
 import kd.bos.dataentity.entity.DynamicObject;
 import kd.bos.dataentity.entity.DynamicObjectCollection;
-import kd.bos.entity.datamodel.IDataModel;
 import kd.bos.entity.operate.result.OperationResult;
-import kd.bos.form.IPageCache;
-import kd.bos.form.control.Control;
+import kd.bos.form.ClientProperties;
 import kd.bos.form.control.EntryGrid;
 import kd.bos.form.control.events.ItemClickEvent;
 import kd.bos.form.field.BasedataEdit;
 import kd.bos.form.field.events.BeforeF7SelectEvent;
 import kd.bos.form.field.events.BeforeF7SelectListener;
-import kd.bos.list.ListShowParameter;
 import kd.bos.orm.query.QCP;
 import kd.bos.orm.query.QFilter;
 import kd.bos.servicehelper.BusinessDataServiceHelper;
-import kd.bos.servicehelper.coderule.CodeRuleServiceHelper;
 import kd.bos.servicehelper.operation.SaveServiceHelper;
 import kd.bos.servicehelper.user.UserServiceHelper;
 import org.apache.commons.lang3.StringUtils;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.EventObject;
 import java.util.List;
@@ -61,35 +57,25 @@ public class GglxFormPlugin extends AbstractBillPlugIn implements BeforeF7Select
         }
     }
 
-//    private String getEntryCode() {
-//        DynamicObject entry = BusinessDataServiceHelper.newDynamicObject("wmq_bidt_entry_code"); //分录规则单据标识
-//        entry.set("wmq_sourceuser", this.getModel().getValue("creator"));
-//        CodeRuleInfo codeRule = CodeRuleServiceHelper.getCodeRule(entry.getDataEntityType().getName(), entry, null);
-//        String entryCode = CodeRuleServiceHelper.getNumber(codeRule, entry);
-//        return entryCode;
-//    }
-
     public void itemClick(ItemClickEvent evt) {
-//        if ("tb_new".equals(evt.getItemKey())) {
-//            DynamicObjectCollection entryentity = this.getModel().getEntryEntity("entryentity");
-//            int currentRow = this.getModel().getEntryCurrentRowIndex("entryentity");
-//            String entryCode = getEntryCode();
-//            this.getModel().setValue("wmq_billnoson", entryCode, currentRow);
-//        }
-
         if ("tb_new".equals(evt.getItemKey())) {
             //新建行
             this.getModel().createNewEntryRow("entryentity");
             int currentRow = this.getModel().getEntryRowCount("entryentity");
+            //单据编号
             String billno = this.getModel().getValue("billno").toString();
-            String i = billno + "_" + currentRow;
-            this.getModel().setValue("wmq_billnoson", i, currentRow - 1);
+
+            //给立项子单号赋值
+            if (currentRow < 10) {
+                this.getModel().setValue("wmq_billnoson", billno + "-0" + currentRow, currentRow - 1);
+            } else {
+                this.getModel().setValue("wmq_billnoson", billno + "-" + currentRow, currentRow - 1);
+            }
         } else if ("tb_del".equals(evt.getItemKey())) {
             DynamicObjectCollection entryentity = this.getModel().getEntryEntity("entryentity");
             String billno = this.getModel().getValue("billno").toString();
             int j = 0;
-            for (int i = 0; i < entryentity.size(); i++) {
-                DynamicObject dynamicObject = entryentity.get(i);
+            for (DynamicObject dynamicObject : entryentity) {
                 String billsub = billno + "_" + ++j;
                 dynamicObject.set("wmq_billnoson", billsub);
             }
@@ -105,28 +91,68 @@ public class GglxFormPlugin extends AbstractBillPlugIn implements BeforeF7Select
                 for (int selectRow : selectRows) {
                     //获取选中行的单据体数据
                     DynamicObject dot = entity.get(selectRow);
-                    //wmq_lx_bill--广告立项单标识
+                    //如选择的分录申请金额-已报金额=0，则该行分录无需清0。
+                    if (dot.getBigDecimal("wmq_apply_amount").subtract(dot.getBigDecimal("wmq_yibao_amount")).compareTo(BigDecimal.ZERO) <= 0) {
+                        this.getView().showTipNotification("第" + (selectRow + 1) + "行申请金额=已报金额无需清0");
+                        continue;
+                    }
+                    //若可用余额已经=0则该行分录无需清0
+                    if (dot.getBigDecimal("wmq_can_balance").compareTo(BigDecimal.ZERO) == 0) {
+                        this.getView().showTipNotification("第" + (selectRow + 1) + "行可用余额已经为0无需清0");
+                        continue;
+                    }
+
+                    //wmq_lx_bill--广告立项单标识  生产一张新的立项申请单
                     DynamicObject data = BusinessDataServiceHelper.newDynamicObject("wmq_lx_bill");
                     //为新增单据头赋值
-                    data.set("billstatus","C");
-                    data.set("wmq_lx_date",this.getModel().getValue("wmq_lx_date"));
-                    data.set("wmq_userfield",this.getModel().getValue("wmq_userfield"));
-                    data.set("wmq_company",this.getModel().getValue("wmq_company"));
-                    data.set("org",this.getModel().getValue("org"));
-                    data.set("pvdo_lxtype","0");
-                    //为新增分录赋值  TODO 标识修改
+                    data.set("billstatus", "C");
+                    data.set("wmq_lx_date", this.getModel().getValue("wmq_lx_date"));
+                    data.set("wmq_userfield", this.getModel().getValue("wmq_userfield"));
+                    data.set("wmq_company", this.getModel().getValue("wmq_company"));
+                    data.set("org", this.getModel().getValue("org"));
+                    data.set("wmq_lx_type", "0");
+                    //会审人
+                    // 获取多选基础资料的值(此时为空)
+                    DynamicObjectCollection currencyColl = data.getDynamicObjectCollection("wmq_reviewer");
+                    // 获取待赋值的币别数据
+                    DynamicObject[] currencyArr = this.getMulbaseDatas();
+                    // 方案一(简单, 推荐)
+                    for (DynamicObject currency : currencyArr) {
+                        DynamicObject newCurrency = new DynamicObject(currencyColl.getDynamicObjectType());
+                        newCurrency.set("fbasedataId", currency);
+                        currencyColl.add(newCurrency);
+                    }
+                    // 多选基础资料字段赋值
+                    data.set("wmq_reviewer", currencyColl);
+
+                    //为新增分录赋值
                     DynamicObjectCollection entryentity = data.getDynamicObjectCollection("entryentity"); //
                     DynamicObject entry = new DynamicObject(entryentity.getDynamicObjectType());
-                    entry.set("pvdo_lxbillno_son",dot.get("pvdo_lxbillno_son"));
-                    entry.set("pvdo_deliverymethod",dot.get("pvdo_deliverymethod"));
-                    entry.set("pvdo_advertisers",dot.get("pvdo_advertisers"));
-                    entry.set("pvdo_product",dot.get("pvdo_product"));
-                    entry.set("pvdo_applyamount",dot.get("pvdo_applyamount"));
-                    entry.set("pvdo_approvedamount",dot.get("pvdo_approvedamount"));
-                    entry.set("pvdo_availablebalance",dot.get("pvdo_availablebalance"));
-                    entry.set("pvdo_amountquoted",dot.get("pvdo_amountquoted"));
-                    entry.set("pvdo_status",dot.get("pvdo_status"));
+                    entry.set("wmq_billnoson", dot.get("wmq_billnoson"));
+                    entry.set("wmq_delivery_method", dot.get("wmq_delivery_method"));
+                    entry.set("wmq_advertiser", dot.get("wmq_advertiser"));
+                    entry.set("wmq_product", dot.get("wmq_product"));
+                    //申请金额=原立项申请单.分录.申请金额-原立项申请单.分录.已报金额
+                    entry.set("wmq_apply_amount", dot.getBigDecimal("wmq_apply_amount").subtract(dot.getBigDecimal("wmq_yibao_amount")));
+                    entry.set("wmq_approval_amount", dot.get("wmq_apply_amount"));
+                    entry.set("wmq_can_balance", dot.get("wmq_can_balance"));
+                    entry.set("wmq_yibao_amount", dot.get(0));
+                    entry.set("wmq_status", "未报");
                     entryentity.add(entry);
+
+                    OperationResult operationResult = SaveServiceHelper.saveOperate("wmq_lx_bill", new DynamicObject[]{data}, OperateOption.create());
+                    if (operationResult.isSuccess()) {//保存成功
+                        //进行提示
+                        this.getView().showTipNotification("生成成功");
+                        //并修改源单对应的分录行的数据
+                        dot.set("wmq_can_balance", 0);
+                        dot.set("wmq_status", "已关闭");
+                        //更新源单的分录行并刷新
+                        SaveServiceHelper.update(dot);
+                        this.getView().updateView("entryentity");
+                    } else {
+                        this.getView().showTipNotification("生成失败");
+                    }
                 }
             }
 
@@ -151,5 +177,55 @@ public class GglxFormPlugin extends AbstractBillPlugIn implements BeforeF7Select
         }
     }
 
+    @Override
+    public void afterBindData(EventObject e) {
+        super.afterBindData(e);
+
+        //单据状态
+        String billstatus = (String) this.getModel().getValue("billstatus");
+
+        java.util.HashMap<String, Object> map = new java.util.HashMap<>();
+        //不同状态设置不同颜色
+        if (billstatus != null) {
+            switch (billstatus) {
+                case "A":  // 保存
+                    map.put(ClientProperties.ForeColor, "pink");
+                    break;
+                case "B":  // 已提交
+                    map.put(ClientProperties.ForeColor, "blue");
+                    break;
+                case "C":  // 审核通过
+                    map.put(ClientProperties.ForeColor, "red");
+                    break;
+                case "D":  // 废弃
+                    map.put(ClientProperties.ForeColor, "green");
+                    break;
+                case "E":  // 关闭
+                    map.put(ClientProperties.ForeColor, "purple");
+                    break;
+                default:
+                    map.put(ClientProperties.ForeColor, "#fffff");
+                    break;
+            }
+        }
+        this.getView().updateControlMetadata("billstatus", map);
+    }
+
+
+    private DynamicObject[] getMulbaseDatas() {
+
+        DynamicObjectCollection coll = (DynamicObjectCollection) getModel().getValue("wmq_reviewer");
+        Object[] idList = new Object[coll.size()];
+
+        for (int i = 0; i < coll.size(); i++) {
+            DynamicObject basedataObj = coll.get(i).getDynamicObject("fbasedataid");
+            Long basedataId = (Long) basedataObj.getPkValue();
+            idList[i] = basedataId;
+        }
+
+        String selectProperties = "id, number, name";
+        QFilter filter = new QFilter("id", QCP.in, idList);
+        return BusinessDataServiceHelper.load("bos_user", selectProperties, filter.toArray());
+    }
 
 }
